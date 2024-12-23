f146c3f09c3a0e8a9aabcfb172c7f0c1c4b3b606
Fontifyai <fontify.ai@gmail.com>
2024-12-23 20:41:41 +0500

    light dark in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Aside from "./_components/Aside";
 import Output from "./_components/Output";
+import Lightdark from "@/components/LandingPage/light-dark";
 
 export default function page() {
   return (
     <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
       <Aside />
       <Output />
+      <Lightdark />
     </div>
   );
 }
